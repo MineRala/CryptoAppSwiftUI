1339f3b46209c40934143eaf0e6b03f96011577e
Mine Rala <mine.rala@rapsodo.com>
2024-04-10 16:04:01 +0300

    Initial Commit

---
+++ CryptoAppSwiftUI/CryptoAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppSwiftUIApp.swift
+//  CryptoAppSwiftUI
+//
+//  Created by Mine Rala on 10.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
