3ae8d6b4a60e942712cd576ca7ca8aa299283ecc
Mine Rala <mine-rala@hotmail.com>
2024-04-19 09:45:09 +0300

    Market Data Service and Model added

--- CryptoAppSwiftUI/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoAppSwiftUI/Core/Home/ViewModels/HomeViewModel.swift
@@ -9,17 +9,13 @@ import Foundation
 import Combine
 
 final class HomeViewModel: ObservableObject  {
-    @Published var statistics = [
-        StatisticModel(title: "Title", value: "Value", percentageChanged: 1),
-        StatisticModel(title: "Title", value: "Value"),
-        StatisticModel(title: "Title", value: "Value"),
-        StatisticModel(title: "Title", value: "Value", percentageChanged: -7)
-    ]
+    @Published var statistics: [StatisticModel] = []
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     @Published var searchText: String = ""
     
-    private let dataService = CoinDataService()
+    private let coinDataService = CoinDataService()
+    private let marketDataService = MarketDataService()
     private var cancallables = Set<AnyCancellable>()
     
     
@@ -28,16 +24,25 @@ final class HomeViewModel: ObservableObject  {
     }
     
     private func addSubscribers() {
-        dataService.$allCoins
+        coinDataService.$allCoins
             .sink { [weak self] coins in
                 guard let self else { return }
                 self.allCoins = coins
             }
             .store(in: &cancallables)
         
+        marketDataService.$marketData
+        /// MarketDataModeli alıp Statistic model'e çevirip(transform) setliyor.
+            .map(mapGlobalMarketData)
+            .sink { [weak self] stats in
+                guard let self else { return }
+                self.statistics = stats
+            }
+            .store(in: &cancallables)
+        
         $searchText
         /// 2 publisher'ı birleştirmek için combine latest kullanılır. Son yayınlanan değeri alır.
-            .combineLatest(dataService.$allCoins)
+            .combineLatest(coinDataService.$allCoins)
         /// yayınlanan değerler arasında bir gecikme (delay) ekler. Bu durumda, arama metni her değiştiğinde işlemin tetiklenmesini biraz geciktirir. Bu, kullanıcı arama metnini hızlıca değiştiriyorsa, her bir değişikliğin hemen işlenmesini önler ve yalnızca belirli bir süre boyunca değişiklik yapılmadığında işlemi gerçekleştirir. Bu, gereksiz iş yükünü azaltmak ve daha verimli bir kullanıcı deneyimi sağlamak için kullanılır.
             .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
             .map(filterCoins)
@@ -56,5 +61,21 @@ final class HomeViewModel: ObservableObject  {
         return coins.filter { coin in
             return coin.name.lowercased().contains(lowercasedText) || coin.symbol.lowercased().contains(lowercasedText) || coin.id.lowercased().contains(lowercasedText)
         }
+    }
+    
+    private func mapGlobalMarketData(data: MarketDataModel?) -> [StatisticModel] {
+        var stats: [StatisticModel] = []
+        guard let data else { return stats }
+        let marketCap = StatisticModel(title: "Market Cap", value: data.marketCap, percentageChanged: data.marketCapChangePercentage24HUsd)
+        let volume = StatisticModel(title: "24h Volume", value: data.volume)
+        let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
+        let portfolio = StatisticModel(title: "Portfolio Value", value: "$0.00", percentageChanged: 0)
+        stats.append(contentsOf: [
+            marketCap,
+            volume,
+            btcDominance,
+            portfolio
+        ])
+        return stats
     }
 }
---
+++ CryptoAppSwiftUI/Services/MarketDataService.swift
@@ -0,0 +1,31 @@
+//
+//  MarketDataService.swift
+//  CryptoAppSwiftUI
+//
+//  Created by Mine Rala on 18.04.2024.
+//
+
+import Foundation
+import Combine
+
+final class MarketDataService {
+    @Published var marketData: MarketDataModel? = nil
+    var marketDataSubscription: AnyCancellable?
+    
+    init() {
+        // bunu bir yerde çağırsa bile marketData dinleyen her yerde data güncellenir.
+        getData()
+    }
+    
+    private func getData() {
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/global") else { return }
+        
+        marketDataSubscription = NetworkingManager.download(url: url)
+            .decode(type: GloabalData.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] globalData in
+                guard let self else { return }
+                self.marketData = globalData.data
+                self.marketDataSubscription?.cancel()
+            })
+    }
+}
